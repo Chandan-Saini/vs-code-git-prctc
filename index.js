b4d68b791dfd3124e45956519592c5fe3f55a8d3
Chandan Kumar <chandan23.saini@gmail.com>
2020-06-07 22:10:17 +0530

    this is my first commit in vscode

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function (req, res) {
+  res.render("home.ejs");
+});
+
+app.listen(3000, process.env.IP, function (req, res) {
+  console.log("server started.........");
+});

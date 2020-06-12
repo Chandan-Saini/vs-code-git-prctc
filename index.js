5b8001aecb230a458a6126b889ad153a1cd86cc7
Chandan Kumar <descentchandan@gmail.com>
2020-06-12 17:26:21 +0530

    practiced js

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-var express = require("express");
-var app = express();
+// var express = require("express");
+// var app = express();
 
-app.get("/", function (req, res) {
-  res.render("landing.ejs");
-});
+// app.get("/", function (req, res) {
+//   res.render("landing.ejs");
+// });
 
-app.get("/home", function (req, res) {
-  res.render("home.ejs");
-});
+// app.get("/home", function (req, res) {
+//   res.render("home.ejs");
+// });
 
-app.listen(3000, process.env.IP, function (req, res) {
-  console.log("server started.........");
-});
+// app.listen(3000, process.env.IP, function (req, res) {
+//   console.log("server started.........");
+// });
+
+var company = new Object();
+company.name = "vaah dot com";
+
+company.ceo = {};
+company.ceo.name="chandan"
+company.$stock="100"
+company["price of stock"]= 200
+console.log(company["price of stock"]);
